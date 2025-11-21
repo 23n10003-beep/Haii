3b7ed2cbcf8573ba05a567712ec2e45df338db59
DanielRio <23n10003@student.unika.ac.id>
2025-11-21 09:52:32 +0700

    Add title to the HTML document

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en"> 
     <head>
+        <title> Coba</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-widt">
         <meta http-equiv="X-UA-Compatible" content="ie">
